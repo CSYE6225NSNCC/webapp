b537711ce31fb9ad8a5039558cbc511ae7c35665
amrutamuzumdar <muzumdar.a@northeastern.edu>
2024-10-17 03:01:33 -0400

    Resolving packer errors

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,16 +2,11 @@
 # scripts/install.sh
 
 # Update package lists
-sudo apt-get update
+sudo apt-get update -y
 
 
 # Install MySQL Server
-sudo apt-get install -y mysql-server
-
-# Install Node.js and npm
-sudo apt-get install -y curl
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - # For Node.js 16.x
-sudo apt-get install nodejs -y
+sudo apt-get install -y mysql-server nodejs npm unzip
 
 # Enable and start MySQL
 sudo systemctl enable mysql
