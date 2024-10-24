01fe404159094931193a9ff835be171c6106aeb6
amrutamuzumdar <muzumdar.a@northeastern.edu>
2024-10-24 02:35:07 -0400

    CHanging permission

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,3 +11,7 @@ sudo apt-get install -y nodejs npm unzip
 # # Enable and start MySQL
 # sudo systemctl enable mysql
 # sudo systemctl start mysql
+
+# Create the user and group csye6225 if they don't exist
+sudo groupadd -f csye6225
+sudo id -u csye6225 &>/dev/null || sudo useradd -m -g csye6225 -s /usr/sbin/nologin csye6225
--- scripts/webapp.sh
+++ scripts/webapp.sh
@@ -1,6 +1,4 @@
-# Create the user and group csye6225 if they don't exist
-sudo groupadd -f csye6225
-sudo id -u csye6225 &>/dev/null || sudo useradd -m -g csye6225 -s /usr/sbin/nologin csye6225
+
 
 
 sudo mkdir -p /opt/webapp
