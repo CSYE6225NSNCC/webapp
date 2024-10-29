575e7cad74207d1a80cb5d440ccd66d82a89aed5
amrutamuzumdar <muzumdar.a@northeastern.edu>
2024-10-29 19:17:18 -0400

    removing config

--- services/photoService.js
+++ services/photoService.js
@@ -1,13 +1,6 @@
 const AWS = require('aws-sdk');
 const  Photo  = require('../models/photoModel.js'); 
 
-// Configure AWS SDK
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-});
-
 const s3 = new AWS.S3();
 
 const uploadOrUpdateProfilePictureService = async (userId, photoFile) => {
